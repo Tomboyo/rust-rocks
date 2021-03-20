1ca6043d7a03a5939ded783f90c0a710067cb5d7
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2021-03-19 21:22:50 -0400

    Remove commented code

--- src/entity/player.rs
+++ src/entity/player.rs
@@ -36,29 +36,3 @@ pub fn new(
         PlayerInput,
     )
 }
-
-// /// Mutate player state based on input events.
-// pub fn consume_input(&mut self, controllers: &impl Controllers, event: &Event) {
-//     match event {
-//         Event::JoyAxisMotion {
-//             which, axis_idx, ..
-//         } => {
-//             self.consume_joystick_input(&controllers.read_joystick(*which, *axis_idx).unwrap());
-//         }
-//         _ => (),
-//     }
-// }
-
-// fn consume_joystick_input(&mut self, joystick: &Joystick) {
-//     match joystick.which_joystick {
-//         WhichJoystick::Left => {
-//             self.velocity.dx = joystick.normal_x() * MAX_SPEED;
-//             self.velocity.dy = joystick.normal_y() * MAX_SPEED;
-//         }
-//         WhichJoystick::Right => {
-//             if let Some(angle) = joystick.angle() {
-//                 self.orientation = angle;
-//             }
-//         }
-//     };
-// }
